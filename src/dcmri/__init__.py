58b006bfe98bd1b92e43067e20886fd7e252324c
plaresmedima <steven.sourbron@googlemail.com>
2024-01-13 13:55:44 +0000

    update __init__

--- src/dcmri/__init__.py
+++ src/dcmri/__init__.py
@@ -3,10 +3,10 @@
 __version__ = "0.6.3"
 
 from dcmri import tools
-from dcmri.tools import *
+from .tools import *
 
 from dcmri import lib
-from dcmri.lib import *
+from .lib import *
 
 from dcmri import pk
-from dcmri.pk import *
+from .pk import *
