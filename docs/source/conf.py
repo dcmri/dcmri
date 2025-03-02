c5f68f71ee5ec6dbf5578b3f786fa5da07f36efe
plaresmedima <steven.sourbron@googlemail.com>
2025-03-02 23:51:38 +0000

    Add google analytics

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -113,7 +113,10 @@ html_theme = 'pydata_sphinx_theme'
 html_theme_options = {
     "github_url": "https://github.com/dcmri/dcmri",
     "collapse_navigation": True,
-    }
+    "analytics": {
+        "google_analytics_id": "G-64ZSJ8Z1MX",
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here, relative to this directory. They are copied after the builtin static files, so a file named "default.css" will overwrite the builtin "default.css"
 html_static_path = ['_static']
