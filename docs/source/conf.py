f25aa090afe3336c7257193c967b86c717f9776d
Steven Sourbron <s.sourbron@sheffield.ac.uk>
2025-04-27 13:43:40 +0100

    increment version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@
 project = 'dcmri'
 copyright = '2024-2025, dcmri contributors'
 author = 'dcmri contributors'
-release = '0.6.16'
+release = '0.6.17'
 
 # -- Path setup --------------------------------------------------------------
 # If extensions (or modules to document with autodoc) are in another directory,
